b5fdff12ab65f173ec281f4b6a27a1db4671f75a
douglas carvalho <bidu620@gmail.com>
2026-09-02 05:41:29 -0300

    GAT Server: versao de arquivo 1.0.42

--- server-local/ui/Properties/AssemblyInfo.cs
+++ server-local/ui/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("GAT_LOG_SERVER")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.41.0")]
-[assembly: AssemblyInformationalVersion("1.0.41.0+go-live-reset")]
+[assembly: AssemblyFileVersion("1.0.42.0")]
+[assembly: AssemblyInformationalVersion("1.0.42.0+central-status-fix")]
 [assembly: AssemblyProduct("GAT_LOG_SERVER")]
 [assembly: AssemblyTitle("GAT_LOG_SERVER")]
-[assembly: AssemblyVersion("1.0.41.0")]
+[assembly: AssemblyVersion("1.0.42.0")]
