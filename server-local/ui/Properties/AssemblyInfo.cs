12e3a403ebc926501dcf69640537000b5815aa5b
douglas carvalho <bidu620@gmail.com>
2026-09-02 22:46:03 -0300

    Painel: versao 1.0.47

--- server-local/ui/Properties/AssemblyInfo.cs
+++ server-local/ui/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("GAT_LOG_SERVER")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.46.0")]
-[assembly: AssemblyInformationalVersion("1.0.46.0+monthly-trip-goal")]
+[assembly: AssemblyFileVersion("1.0.47.0")]
+[assembly: AssemblyInformationalVersion("1.0.47.0+lapeal-rank-repair")]
 [assembly: AssemblyProduct("GAT_LOG_SERVER")]
 [assembly: AssemblyTitle("GAT_LOG_SERVER")]
-[assembly: AssemblyVersion("1.0.46.0")]
+[assembly: AssemblyVersion("1.0.47.0")]
