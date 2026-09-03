8a91f3f9fbdff9e5047448cfe7c6e41f8a9bb697
douglas carvalho <bidu620@gmail.com>
2026-09-03 13:14:57 -0300

    Painel: versao 1.0.48 JeanJC

--- server-local/ui/Properties/AssemblyInfo.cs
+++ server-local/ui/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("GAT_LOG_SERVER")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.47.0")]
-[assembly: AssemblyInformationalVersion("1.0.47.0+lapeal-rank-repair")]
+[assembly: AssemblyFileVersion("1.0.48.0")]
+[assembly: AssemblyInformationalVersion("1.0.48.0+jeanjc-rank-repair")]
 [assembly: AssemblyProduct("GAT_LOG_SERVER")]
 [assembly: AssemblyTitle("GAT_LOG_SERVER")]
-[assembly: AssemblyVersion("1.0.47.0")]
+[assembly: AssemblyVersion("1.0.48.0")]
