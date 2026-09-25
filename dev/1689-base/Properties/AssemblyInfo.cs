90fac7bf6d691b68c588df88355c17ef14234bd7
douglas carvalho <bidu620@gmail.com>
2026-09-24 21:34:19 -0300

    Set GAT Telemetria file version to 1.0.68.9

--- dev/1689-base/Properties/AssemblyInfo.cs
+++ dev/1689-base/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("GAT_TELEMETRIA")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.32.0")]
-[assembly: AssemblyInformationalVersion("1.0.32.0+gat-damage-v3")]
+[assembly: AssemblyFileVersion("1.0.68.9")]
+[assembly: AssemblyInformationalVersion("1.0.68.9+radio-live-voice-clean")]
 [assembly: AssemblyProduct("GAT_TELEMETRIA")]
 [assembly: AssemblyTitle("GAT_TELEMETRIA")]
-[assembly: AssemblyVersion("1.0.32.0")]
+[assembly: AssemblyVersion("1.0.68.9")]
 
